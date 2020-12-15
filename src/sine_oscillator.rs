0e09ca8c98b6f43a1bf0d31ba3459f725fa8694a
Eric Wood <eric@ericwood.org>
2020-12-15 09:14:52 -0700

    Optimize sine function, remove unnecessary branching

--- src/sine_oscillator.rs
+++ src/sine_oscillator.rs
@@ -2,8 +2,8 @@ use std::f32::consts;
 
 pub struct SineOscillator {
   sample_rate: f32,
-  current: f32,
-  rate: f32,
+  current_phase: f32,
+  step: f32,
   max: f32,
 }
 
@@ -11,24 +11,20 @@ impl SineOscillator {
   pub fn new(sample_rate: f32, rate: f32) -> Self {
     SineOscillator {
       sample_rate,
-      rate: 2.0 * consts::PI * (rate / sample_rate),
+      step: rate / sample_rate,
       max: 2.0 * consts::PI,
-      current: 0.0,
+      current_phase: 0.0,
     }
   }
 
   pub fn set(&mut self, rate: f32) {
-    self.rate = self.max * (rate / self.sample_rate);
+    self.step = rate / self.sample_rate;
   }
 
   #[inline]
   pub fn process(&mut self) -> f32 {
-    self.current += self.rate;
+    self.current_phase = (self.current_phase + self.step).fract();
 
-    if self.current > self.max {
-      self.current -= self.max;
-    }
-
-    self.current.sin()
+    (self.current_phase * self.max).sin()
   }
 }
