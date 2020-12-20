93991bb0d64ea67451def5964622ebf65b151bff
Eric Wood <eric@ericwood.org>
2020-12-19 20:45:12 -0600

    Add run script for fun

---
+++ run.sh
@@ -0,0 +1,8 @@
+# Meant for building and running in such a way that I can actually
+# read what's being printed in stdout for when things are especially jank
+
+set -ex
+
+./build.sh
+
+/Applications/Ableton\ Live\ 10\ Suite.app/Contents/MacOS/Live testing/project/isolated.als
